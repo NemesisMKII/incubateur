076bf1a6fdb35acece0ab3638ed0c2f0a2aba7a9
Bernard <leborgne.bernard62@gmail.com>
2022-03-16 16:15:24 +0100

    first commit

---
+++ React - Tailwind/mcdalle/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App bg-green-100 h-screen flex items-center">
+      <div className='container bg-white h-4/5 w-2/3 mx-auto rounded-xl'>
+        <div className='header'>
+          <ul className='flex text-3xl place-content-center space-x-10 mt-3'>
+            <li>Menus</li>
+            <li>Menus</li>
+            <li>Menus</li>
+            <li>Menus</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
